cac864fd89b7f2edf16fa4e7658ce8108a0df237
michaelCSBBA <h86lin@uwaterloo.ca>
2022-06-21 00:52:13 -0400

    Added Basic Game Logic, Made Rough Game Session UI Design

--- client_app/app/src/main/java/com/spellingo/client_app/GameSessionViewModel.kt
+++ client_app/app/src/main/java/com/spellingo/client_app/GameSessionViewModel.kt
@@ -5,10 +5,12 @@ class GameSessionViewModel() : ViewModel() {
     val model = Model()
 
 
+    // startGame() -> Starts the game and loads the list of words
     fun startGame () {
         model.startGame()
     }
 
+    // getWord(): String -> Returns a randomly generated word from the model
     fun getWord () : String{
         return model.getRandomWord()
     }
