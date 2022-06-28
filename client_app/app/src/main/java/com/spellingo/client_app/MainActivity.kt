7a44e2421179a4d2c18e36406b103212ff1def9c
Andrew Chin <42951909+CorrectButNotRight@users.noreply.github.com>
2022-06-27 23:34:01 -0400

    Moved word retrieval from server to MainActivity (#23)

--- client_app/app/src/main/java/com/spellingo/client_app/MainActivity.kt
+++ client_app/app/src/main/java/com/spellingo/client_app/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toolbar
+import androidx.activity.viewModels
 import com.google.android.material.internal.ContextUtils.getActivity
 import com.spellingo.client_app.databinding.ActivityMainBinding
 
@@ -22,5 +23,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Application startup tasks handled in Activity's ViewModel
+        val viewModel: MainActivityViewModel by viewModels()
+        viewModel.startApp()
     }
 }
--- client_app/app/src/main/java/com/spellingo/client_app/MainMenuViewModel.kt
+++ client_app/app/src/main/java/com/spellingo/client_app/MainMenuViewModel.kt
@@ -3,18 +3,8 @@ package com.spellingo.client_app
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 
 class MainMenuViewModel(application: Application) : AndroidViewModel(application) {
-    private val model = UpdateModel(application)
-    fun startApp() {
-        viewModelScope.launch {
-            try {
-                model.generateWords()
-            }
-            catch(e: Exception) {
-                System.err.println(e.toString())
-            }
-        }
-    }
 }
