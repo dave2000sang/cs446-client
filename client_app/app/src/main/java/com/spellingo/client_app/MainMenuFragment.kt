308ed23d7fca40265fd21571d55e6a9562f86387
michaelCSBBA <h86lin@uwaterloo.ca>
2022-07-06 02:10:44 -0400

    PieChart Done

--- client_app/app/src/main/java/com/spellingo/client_app/MainMenuFragment.kt
+++ client_app/app/src/main/java/com/spellingo/client_app/MainMenuFragment.kt
@@ -32,6 +32,12 @@ class MainMenuFragment : Fragment() {
             findNavController().navigate(R.id.mainMenuFragment_to_settingsFragment)
         }
 
+        val statsButton = root.findViewById<Button>(R.id.statsButton)
+        statsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_fragment_main_menu_to_statisticsFragment)
+        }
+
+
         return root
     }
 
---
+++ client_app/app/src/main/java/com/spellingo/client_app/StatViewModel.kt
@@ -0,0 +1,10 @@
+package com.spellingo.client_app
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class StatViewModel(application: Application) : AndroidViewModel(application) {
+
+}
