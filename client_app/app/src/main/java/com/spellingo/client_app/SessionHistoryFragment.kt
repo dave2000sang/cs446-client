01c428ded02c2da3fc3cf0d28bbdfac12dbcf80d
michaelCSBBA <h86lin@uwaterloo.ca>
2022-07-10 17:12:17 -0400

    Added Basic Session Entries into Session History UI

--- client_app/app/src/main/java/com/spellingo/client_app/SessionHistoryFragment.kt
+++ client_app/app/src/main/java/com/spellingo/client_app/SessionHistoryFragment.kt
@@ -1,11 +1,15 @@
 package com.spellingo.client_app
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -23,14 +27,21 @@ class SessionHistoryFragment: Fragment() {
 
     private val viewModel: StatisticsViewModel by activityViewModels()
 
+    @SuppressLint("ResourceType")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val root = inflater.inflate(R.layout.fragment_session_history, container, false)
+        val sessionList = root.findViewById<LinearLayout>(R.id.scrollLinearLayout)
+        val view = inflater.inflate(R.layout.session_entry_item, null)
+        val view1 = inflater.inflate(R.layout.session_entry_item, null)
+        val view2 = inflater.inflate(R.layout.session_entry_item, null)
 
 
-
+        sessionList.addView(view)
+        sessionList.addView(view1)
+        sessionList.addView(view2)
         return root
     }
 }
